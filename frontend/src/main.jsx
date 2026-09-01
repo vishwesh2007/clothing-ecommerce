d7a4e2d752dcf14e5009e82f6c782a9760d638cd
Vishwesh Sitapara <vishweshsitapara2007@gmail.com>
2026-09-01 16:05:44 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 import ProductsProvider from "./context/Products.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
-  <>
-    <BrowserRouter>
-      <ProductsProvider>
-        <App />
-      </ProductsProvider>
-    </BrowserRouter>
-  </>
+  <BrowserRouter>
+    <ProductsProvider>
+      <App />
+      <SpeedInsights />
+    </ProductsProvider>
+  </BrowserRouter>
 );
