f02be8abf6cbba57e52d5e9b376426c94ea3f2ef
Vishwesh Sitapara <vishweshsitapara2007@gmail.com>
2026-09-22 17:53:36 +0530

    Fix middleware folder casing

--- backend/Middleware/authMiddleware.js
+++ backend/Middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { User } from "../model/user.model.js"; // 👈 Apne model ka sahi path check kar lena
+import { User } from "../model/user.model.js"; 
 
 const authMiddleware = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
